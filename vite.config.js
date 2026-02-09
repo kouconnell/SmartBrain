2b186f66996dad7ced9e640137d01fd19a2feda1
ck2mcv <ck2mcv@virginia.edu>
2026-02-09 13:59:41 -0500

    reverting back changes

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/SmartBrain/',
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
+  server: {
+    open: true,
   }
 })
